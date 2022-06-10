fdc83026a47904541a204a917f4191c43bc902c2
Jkalagate <jakub.kalagate@gmail.com>
2022-06-10 14:56:09 +0200

    Add new currency -> Franc
    i have added new class Franc and i did the same test what
    class Dollat has

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -7,8 +7,8 @@ class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        assertEquals(10, five.times(2));
-        assertEquals(15, five.times(3));
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
