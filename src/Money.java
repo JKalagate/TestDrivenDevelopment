05acf3600724468c8d406152858459e903f64bb7
Jkalagate <jakub.kalagate@gmail.com>
2022-06-14 02:09:02 +0200

    Transfer the same functions to the highest class
    I have created static funs for objcets and abstract for
    multiplying funs. Now tests refer to Money class

--- src/Money.java
+++ src/Money.java
@@ -1,4 +1,4 @@
-public class Money {
+abstract class Money {
     protected int amount;
 
     public boolean equals (Object object){
@@ -7,4 +7,12 @@ public class Money {
                 getClass().equals(money.getClass());
     }
 
+    abstract Money times (int multiplier);
+
+    static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+    static Money franc(int amount){
+        return new Franc(amount);
+    }
 }
